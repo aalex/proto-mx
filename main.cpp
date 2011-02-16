d6208175e62bc04ff2bc4e61026f956c89a55cc0
Alexandre Quessy <alexandre@quessy.net>
2011-02-16 10:36:19 +0100

    using a JSON script for layout

--- main.cpp
+++ main.cpp
@@ -4,19 +4,63 @@
 #include <clutter/clutter.h>
 #include <mx/mx.h>
 
+#define GUI_SCRIPT "mockup.json"
+
+gboolean state_matches_string(ClutterState *state, gchar *name)
+{
+    return g_strcmp0(clutter_state_get_state(state), name) == 0;
+}
+
+/**
+ * Loads a ClutterScript from a file.
+ */
+ClutterScript *require_script(const gchar *filename)
+{
+    GError *error = NULL;
+    ClutterScript *script = clutter_script_new();
+    clutter_script_load_from_file(script, filename, &error);
+    if (error)
+    {
+        g_critical("Error loading JSON script %s: %s", filename, error->message);
+        g_error_free(error);
+        g_object_unref(script);
+        script = NULL;
+    }
+    return script;
+}
+
+/**
+ * Loads a GObject from a ClutterScript.
+ */
+GObject *require_object_from_script(ClutterScript *script, const gchar *name)
+{
+    GObject *obj = NULL;
+    obj = clutter_script_get_object(script, name);
+    if (obj == NULL) 
+    {
+        gboolean filename_set;
+        gchar *filename;
+
+        g_object_get(G_OBJECT(script), "filename-set", &filename_set, "filename", &filename, NULL);
+        g_critical("No object %s found in clutter script %s.", name, filename_set ? filename : "unknown");
+        g_free(filename);
+    }
+    return obj;
+}
+
 static const gint WIN_W = 640;
 static const gint WIN_H = 480;
 
-typedef struct stuff_
+typedef struct assistant_
 {
-    ClutterActor *group;
-    ClutterActor *slider;
-} Stuff;
+    ClutterActor *stage;
+    ClutterScript *script;
+} Assistant;
 
 static void key_event_cb(ClutterActor *actor, ClutterKeyEvent *event, gpointer data)
 {
-    Stuff *self = (Stuff *) data;
-    (void) self; // unused
+    Assistant *assistant = (Assistant *) data;
+    (void) assistant; // unused
     switch (event->keyval)
     {
         case CLUTTER_Escape:
@@ -28,16 +72,16 @@ static void key_event_cb(ClutterActor *actor, ClutterKeyEvent *event, gpointer d
             break;
     }
 }
-
+/*
 static void on_button_clicked(ClutterActor *actor, gpointer data)
 {
-    Stuff *self = (Stuff *) data;
-    (void) self; // unused
+    Assistant *assistant = (Assistant *) data;
+    (void) assistant; // unused
     (void) actor; // unused
     g_print("clicked\n");
-    g_print("slider value: %f\n", mx_slider_get_value(MX_SLIDER(self->slider)));
+    //g_print("slider value: %f\n", mx_slider_get_value(MX_SLIDER(assistant->slider)));
 }
-
+*/
 int main(int argc, char *argv[])
 {
     clutter_init(&argc, &argv);
@@ -49,48 +93,15 @@ int main(int argc, char *argv[])
     clutter_stage_set_color(CLUTTER_STAGE(stage), &black);
     clutter_actor_set_size(stage, WIN_W, WIN_H);
 
-    Stuff *self = g_new0(Stuff, 1);
-    ClutterLayoutManager *layout = clutter_flow_layout_new(CLUTTER_FLOW_HORIZONTAL);
-    clutter_flow_layout_set_row_spacing(CLUTTER_FLOW_LAYOUT(layout), 4.0f);
-    clutter_flow_layout_set_column_spacing(CLUTTER_FLOW_LAYOUT(layout), 4.0f);
-    self->group = clutter_box_new(layout);
-    clutter_actor_set_width(self->group, clutter_actor_get_width(stage));
-    clutter_container_add_actor(CLUTTER_CONTAINER(stage), self->group);
-    clutter_actor_set_y(self->group, 8.0f);
-    
-    // TOOLBAR
-    ClutterActor *toolbar = mx_toolbar_new();
-    mx_toolbar_set_has_close_button(MX_TOOLBAR(toolbar), FALSE);
-    clutter_actor_set_width(toolbar, WIN_W);
-    ClutterActor *tools = mx_box_layout_new();
-    ClutterActor *button_a = mx_button_new_with_label("Click A!");
-    ClutterActor *button_b = mx_button_new_with_label("Click B!");
-    mx_box_layout_add_actor(MX_BOX_LAYOUT(tools), button_b, 0);
-    mx_box_layout_add_actor(MX_BOX_LAYOUT(tools), button_a, 0);
-    mx_bin_set_child(MX_BIN(toolbar), tools), 
-    clutter_container_add_actor(CLUTTER_CONTAINER(self->group), toolbar);
-
-    // RECTANGLES
-    int i;
-    ClutterColor orange = { 0xff, 0xcc, 0x33, 0xff };
-    for (i = 0; i < 10; ++i)
-    {
-        ClutterActor *actor = clutter_rectangle_new_with_color(&orange);
-        gfloat SIZE = 80.0f;
-        clutter_actor_set_size(actor, SIZE, SIZE);
-        clutter_container_add_actor(CLUTTER_CONTAINER(self->group), actor);
-    }
-
-    // SLIDER + BUTTON
-    ClutterActor *slider = mx_slider_new();
-    ClutterActor *button = mx_button_new_with_label("Click me!");
-    clutter_container_add_actor(CLUTTER_CONTAINER(self->group), slider);
-    clutter_container_add_actor(CLUTTER_CONTAINER(self->group), button);
-    self->slider = slider;
-    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), self);
+    Assistant *assistant = g_new0(Assistant, 1);
+    assistant->script = require_script(GUI_SCRIPT);
+    assistant->stage = stage;
+    ClutterActor *root = CLUTTER_ACTOR(require_object_from_script(assistant->script, "root"));
+    clutter_container_add_actor(CLUTTER_CONTAINER(stage), root);
+    //g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), assistant);
 
     // DONE
-    g_signal_connect(stage, "key-press-event", G_CALLBACK(key_event_cb), self);
+    g_signal_connect(stage, "key-press-event", G_CALLBACK(key_event_cb), assistant);
     clutter_actor_show(stage);
     clutter_main();
     return 0;
